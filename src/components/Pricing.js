4b426c72c5b62729e7e3a1161f3fae701d1a52af
solopreneuranas <solopreneuranas@gmail.com>
2023-10-16 08:52:49 +0530

    v-7.3.3

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import '../App.css';
 
+import Header from './Header';
+import Footer from './Footer';
+import CTA from './CTA';
 import { Grid, Button, Typography } from "@mui/material";
 import DoneIcon from '@mui/icons-material/Done';
 import Modal from '@mui/material/Modal';
@@ -105,75 +108,83 @@ export default function Pricing() {
 
 
     return (
-        <Grid container spacing={5} className='pricingPackage' style={{ marginBottom: '5%' }}>
-            <Grid item xs={12}>
-                <center>
-                    <h2 className='featuresHeading'>Simplified<span style={{ color: '#FF6326' }}> Pricing </span>for all your needs</h2>
-                    <p className='featuresPara' style={{ marginTop: '1%', marginBottom: '1%' }}>Get upfront, clear pricing for starting and running your business.</p><br />
-                </center>
-            </Grid>
-            <Grid item md={6} style={{ width: '100%' }}>
-                <Grid container spacing={0} className='business'>
-                    <Grid item xs={12} className='tabLeftCol'>
-                        <h3 style={{ fontWeight: 600, fontSize: '20px', marginBottom: '2%' }}>Business</h3>
-                        <h3 style={{ fontWeight: 600, fontSize: '40px' }}>$199 <span style={{ fontWeight: 400, fontSize: '20px' }}>+ State Fees</span></h3>
-                        <p className='pricingContent' style={{ marginTop: '2%' }}>If you're operating with a low budget.</p>
-                        <Button onClick={handleOpen} fullWidth variant='contained' className='globalButton' style={{
-                            background: '#FF6326',
-                            padding: '3% 4%',
-                            margin: '7% 0 5%',
-                            fontFamily: 'Inter'
-                        }}>Go Business</Button>
+        <div>
+            <Header />
 
-                        {
-                            businessItems.map((item, i) => {
-                                return (
+            <Grid container spacing={5} className='pricingPackage' style={{ marginBottom: '5%' }}>
+                <Grid item xs={12}>
+                    <center>
+                        <h2 className='featuresHeading'>Simplified<span style={{ color: '#FF6326' }}> Pricing </span>for all your needs</h2>
+                        <p className='featuresPara' style={{ marginTop: '1%', marginBottom: '1%' }}>Get upfront, clear pricing for starting and running your business.</p><br />
+                    </center>
+                </Grid>
+                <Grid item md={6} style={{ width: '100%' }}>
+                    <Grid container spacing={0} className='business'>
+                        <Grid item xs={12} className='tabLeftCol'>
+                            <h3 style={{ fontWeight: 600, fontSize: '20px', marginBottom: '2%' }}>Business</h3>
+                            <h3 style={{ fontWeight: 600, fontSize: '40px' }}>$199 <span style={{ fontWeight: 400, fontSize: '20px' }}>+ State Fees</span></h3>
+                            <p className='pricingContent' style={{ marginTop: '2%' }}>If you're operating with a low budget.</p>
+                            <Button onClick={handleOpen} fullWidth variant='contained' className='globalButton' style={{
+                                background: '#FF6326',
+                                padding: '3% 4%',
+                                margin: '7% 0 5%',
+                                fontFamily: 'Inter'
+                            }}>Go Business</Button>
 
-                                    <Grid container spacing={1} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '0' }}>
-                                        <Grid item xs={12} style={{ display: 'flex', justifyContent: 'left', alignItems: 'center' }}>
-                                            <span style={{ marginRight: '2%' }}><DoneIcon fontSize='large' /></span>
-                                            <span className='pricingContent' style={{ fontSize: '18px', padding: 0, margin: 0 }}>{item.title}</span>
+                            {
+                                businessItems.map((item, i) => {
+                                    return (
+
+                                        <Grid container spacing={1} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '0' }}>
+                                            <Grid item xs={12} style={{ display: 'flex', justifyContent: 'left', alignItems: 'center' }}>
+                                                <span style={{ marginRight: '2%' }}><DoneIcon fontSize='large' /></span>
+                                                <span className='pricingContent' style={{ fontSize: '18px', padding: 0, margin: 0 }}>{item.title}</span>
+                                            </Grid>
                                         </Grid>
-                                    </Grid>
-                                )
-                            })
-                        }
+                                    )
+                                })
+                            }
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
 
-            <Grid item md={6} style={{ width: '100%' }}>
-                <Grid container spacing={0} className='premium'>
-                    <Grid item xs={12} className='tabLeftCol'>
-                        <h3 style={{ fontWeight: 600, fontSize: '20px', marginBottom: '2%' }}>Premium</h3>
-                        <h3 style={{ fontWeight: 600, fontSize: '40px' }}>$299 <span style={{ fontWeight: 400, fontSize: '20px' }}>+ State Fees</span></h3>
-                        <p className='pricingContent' style={{ marginTop: '2%' }}>Enhanced, fast, and exclusive service.</p>
-                        <Button onClick={handleOpen} fullWidth variant='contained' className='globalButton' style={{
-                            background: 'white',
-                            color: 'black',
-                            padding: '3% 4%',
-                            margin: '7% 0 5%',
-                            fontFamily: 'Inter'
-                        }}>Go Premium</Button>
+                <Grid item md={6} style={{ width: '100%' }}>
+                    <Grid container spacing={0} className='premium'>
+                        <Grid item xs={12} className='tabLeftCol'>
+                            <h3 style={{ fontWeight: 600, fontSize: '20px', marginBottom: '2%' }}>Premium</h3>
+                            <h3 style={{ fontWeight: 600, fontSize: '40px' }}>$299 <span style={{ fontWeight: 400, fontSize: '20px' }}>+ State Fees</span></h3>
+                            <p className='pricingContent' style={{ marginTop: '2%' }}>Enhanced, fast, and exclusive service.</p>
+                            <Button onClick={handleOpen} fullWidth variant='contained' className='globalButton' style={{
+                                background: 'white',
+                                color: 'black',
+                                padding: '3% 4%',
+                                margin: '7% 0 5%',
+                                fontFamily: 'Inter'
+                            }}>Go Premium</Button>
 
-                        {
-                            premiumItems.map((item, i) => {
-                                return (
+                            {
+                                premiumItems.map((item, i) => {
+                                    return (
 
-                                    <Grid container spacing={1} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '0' }}>
-                                        <Grid item xs={12} style={{ display: 'flex', justifyContent: 'left', alignItems: 'center' }}>
-                                            <span style={{ marginRight: '2%' }}><DoneIcon fontSize='large' /></span>
-                                            <span className='pricingContent' style={{ fontSize: '18px', padding: 0, margin: 0 }}>{item.title}</span>
+                                        <Grid container spacing={1} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '0' }}>
+                                            <Grid item xs={12} style={{ display: 'flex', justifyContent: 'left', alignItems: 'center' }}>
+                                                <span style={{ marginRight: '2%' }}><DoneIcon fontSize='large' /></span>
+                                                <span className='pricingContent' style={{ fontSize: '18px', padding: 0, margin: 0 }}>{item.title}</span>
+                                            </Grid>
                                         </Grid>
-                                    </Grid>
-                                )
-                            })
-                        }
+                                    )
+                                })
+                            }
+                        </Grid>
                     </Grid>
                 </Grid>
+
+                {pricingModal()}
             </Grid>
-            
-            {pricingModal()}
-        </Grid>
+
+            <CTA />
+            <Footer />
+
+        </div>
     );
 }
