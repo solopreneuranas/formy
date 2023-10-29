16fc6cdebde1ab44c0de3086cdae6b8396f70b30
solopreneuranas <solopreneuranas@gmail.com>
2023-10-29 11:34:36 +0530

    v-7.5.2

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,3 +1,34 @@
+// import * as React from 'react';
+
+// import { Grid} from "@mui/material";
+
+// export default function Brands() {
+//     return (
+//         <div className='root'>
+
+//             <Grid container spacing={5} className='brandsBox'>
+//                 <Grid item>
+//                     <img src='/images/stripe-logo.png' className='logo'/>
+//                 </Grid>
+//                 <Grid item>
+//                     <img src='/images/mercury-logo.svg' className='logo'/>
+//                 </Grid>
+//                 <Grid item>
+//                     <img src='/images/wise-logo.svg' className='logo'/>
+//                 </Grid>
+//                 <Grid item>
+//                     <img src='/images/payoneer-logo.svg' className='logo'/>
+//                 </Grid>
+//                 <Grid item>
+//                     <img src='/images/relay-logo.svg' className='logo'/>
+//                 </Grid>
+//             </Grid>
+
+//         </div>
+//     )
+// }
+
+
 import React, { useRef } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -20,8 +51,8 @@ export default function Brands() {
         speed: 100,
         focusOnSelect: false,
         autoplay: true,
-        slidesToShow: matches ? 3 : 4,
-        slidesToScroll: matches ? 3 : 4,
+        slidesToShow: matches ? 2 : 4,
+        slidesToScroll: matches ? 2 : 4,
         arrows: false,
     };
 
